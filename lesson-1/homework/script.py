d6902612c655a59fa6b95be797c574186021713d
JShKamolbek <110083178+JShKamolbek@users.noreply.github.com>
2025-08-09 11:29:43 +0500

    Update script.py

--- lesson-1/homework/script.py
+++ lesson-1/homework/script.py
@@ -1,1 +1,19 @@
+# 1.
+a = 5
+print(4 * a)
+print(a * a)
 
+# 2.
+import math
+d = 10
+print(math.pi * d)
+
+# 3.
+a = 100
+b = 200
+print((a + b) / 2)
+
+# 4.
+a = 12
+b = 14
+print(a+b, a*b, a*a, b*b)
