52c7667a76936dbd54f8c543a7ecc8fa3843a366
JShKamolbek <110083178+JShKamolbek@users.noreply.github.com>
2025-08-10 20:23:20 +0500

    Update main.py

--- lesson-3/homework3/main.py
+++ lesson-3/homework3/main.py
@@ -1,1 +1,14 @@
+## 1 Create and Access List Elements (Create a list containing five different fruits and print the third fruit.)
+ls = ["Olma", "Nok", "Banan", "Behi", "Uzum"]
+print(ls[3])
 
+## 2. Concatenate Two Lists (Create two lists of numbers and concatenate them into a single list.)
+ls1 = [1, 2, 3, 4]
+ls2 = [6, 5, 4, 3]
+ls1.extend(ls2)
+ls1
+
+## 3. Extract Elements from a List (Given a list of numbers, extract the first, middle, and last elements and store them in a new list.)
+ls = ["Salom", 3, "men", 88, "dasturchiman"]
+ls2 = [ls[0], ls[len(ls)//2], ls[len(ls)-1]]
+ls2
